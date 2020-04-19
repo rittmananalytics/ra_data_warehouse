a8e311aea5872baafa20d2a0a8afb05fe0c98161
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-04-19 18:27:48 +0100

    added gcp billing export sde

--- ra_biapps/models/sde_adapters/sde_jira_projects/sde_jira_projects_tasks.sql
+++ ra_biapps/models/sde_adapters/sde_jira_projects/sde_jira_projects_tasks.sql
@@ -12,7 +12,9 @@ with source as (
   FROM (
     SELECT
       *,
-      MAX(_sdc_batched_at) OVER (PARTITION BY id ORDER BY _sdc_batched_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS max_sdc_batched_at
+      MAX(_sdc_batched_at)
+       OVER (PARTITION BY id ORDER BY _sdc_batched_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+       AS max_sdc_batched_at
     FROM
       {{ source('jira', 'issues') }})
   WHERE
