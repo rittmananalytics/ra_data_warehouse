9b46ffe5018d414d4ff23229402fbb1afb04d069
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-06-14 16:32:29 +0100

    Update wh_timesheet_rpt.sql

--- models/warehouse/w_projects/wh_timesheet_rpt.sql
+++ models/warehouse/w_projects/wh_timesheet_rpt.sql
@@ -17,65 +17,51 @@ company_name,
 company_description,
 timesheet_project_pk,
 exchange_rate,
+total_revenue as total_revenue,
+project_budget_amount,
 project_name,
-project_duration_months,
-task_name,
+project_duration_weeks,
 project_code,
 max(project_delivery_start_ts) as project_delivery_start_ts ,
 max(project_is_active) as project_is_active,
 max(project_budget_by) as project_budget_by,
-round(safe_divide(round(max(project_fee_amount)*exchange_rate),project_duration_months)) as project_fee_amount,
-round(safe_divide(round(max(project_budget_amount)*exchange_rate),project_duration_months)) as project_budget_amount,
+max(round((project_fee_amount*exchange_rate))) as project_fee_amount_gbp,
 max(project_is_fixed_fee) as project_is_fixed_fee,
 round(sum(total_hours_billed)) as total_hours_billed,
-round(sum(round(consultant_share_revenue))*exchange_rate) as total_revenue,
-round(sum(round(consultant_cost))*exchange_rate) as total_consultant_cost,
-round(sum(round(consultant_contribution))*exchange_rate) as total_consultant_contribution,
-round(sum(round(consultant_share_revenue))/sum(total_hours_billed)*exchange_rate,0) as effective_hourly_rate,
-round(safe_divide(sum(round(consultant_contribution)),sum(round(consultant_share_revenue))),2) as project_margin_pct,
-array_agg(struct(timesheet_billing_month,user_name,task_name,user_is_contractor,consultant_hours_billed,consultant_share_hours_billed,round(consultant_share_revenue) as consultant_share_local_revenue,round(consultant_cost) as consultant_local_cost,round(consultant_contribution) as consultant_local_contribution, round(consultant_effective_hourly_rate) as consultant_effective_local_hourly_rate, round(consultant_margin_pct,2) as consultant_margin_pct ) ) consultant
+round(sum(round(consultant_cost*exchange_rate))) as total_consultant_cost_gbp,
+round((total_revenue*exchange_rate)-sum(consultant_cost*exchange_rate)) as total_project_margin_gbp,
+round((total_revenue*exchange_rate)/sum(total_hours_billed)) as effective_hourly_rate_gbp,
+safe_divide((total_revenue*exchange_rate)-sum(consultant_cost*exchange_rate),(total_revenue*exchange_rate)) as project_margin,
+round((sum(total_hours_billed)/project_budget_amount),2) as project_hours_vs_budget,
+round(round(sum(round(consultant_cost*exchange_rate))) * round((sum(total_hours_billed)/project_budget_amount),2) - round(sum(round(consultant_cost*exchange_rate)))) as consulting_cost_budget_variance_gbp,
+array_agg(struct(timesheet_billing_week,user_name,task_name,user_is_contractor,consultant_hours_billed,round(consultant_share_hours_billed_pct,2) as consultant_share_hours_billed_pct,
+  round((total_revenue*exchange_rate)*consultant_share_hours_billed_pct) as consultant_share_total_revenue_gbp,
+          round(consultant_cost*exchange_rate) as consultant_cost_gbp, round( (total_revenue*consultant_share_hours_billed_pct) - (consultant_cost*exchange_rate)) as consultant_contribution_gbp,
+          round(safe_divide(((total_revenue*consultant_share_hours_billed_pct)-consultant_cost) , (total_revenue*consultant_share_hours_billed_pct)),2) as consultant_margin_pct ) ) consultant
 from (
 SELECT *,
-  case when company_currency_code = 'USD' then 1
-       when company_currency_code = 'EUR' then 1
+  case when company_currency_code = 'USD' then 1.0
+       when company_currency_code = 'EUR' then 1.0
        else 1 end as exchange_rate,
-       count(timesheet_billing_month) over (PARTITION BY timesheet_project_pk, timesheet_billing_month) as project_duration_months,
-  round(SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month)) AS total_project_hours_billed,
-  round(SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) as consultant_hours_billed,
-  case when user_is_contractor and project_is_fixed_fee then ((project_fee_amount/(count(timesheet_billing_month) over (PARTITION BY timesheet_project_pk, timesheet_billing_month)))/2)
-       when user_is_contractor and not project_is_fixed_fee then (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *50
-       when user_name = 'Mark Rittman' then (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *50
-       else (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *25 end as consultant_cost,
-  round(total_hours_billed/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month),2) AS consultant_share_hours_billed,
-
-  (round((total_revenue/count(timesheet_billing_month) over (PARTITION BY timesheet_project_pk, timesheet_billing_month)) * (total_hours_billed/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month)),2)) as consultant_share_revenue,
-
-  (round((total_revenue/count(timesheet_billing_month) over (PARTITION BY timesheet_project_pk, timesheet_billing_month)) * (total_hours_billed/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month)),2)) -
-
-  (case when user_is_contractor and project_is_fixed_fee then ((project_fee_amount/(count(timesheet_billing_month) over (PARTITION BY timesheet_project_pk, timesheet_billing_month)))/2)
-       when user_is_contractor and not project_is_fixed_fee then (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *50
-       when user_name = 'Mark Rittman' then (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *50
-       else (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *25 end) as consultant_contribution,
-
-
-       (round((total_revenue/count(timesheet_billing_month) over (PARTITION BY timesheet_project_pk, timesheet_billing_month)) * (total_hours_billed/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month)),2)) /
-       SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month ) as consultant_effective_hourly_rate,
-       safe_divide(((round(total_revenue * (total_hours_billed/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month)),2)) -
-  (case when user_is_contractor and project_is_fixed_fee then project_fee_amount/2
-       when user_is_contractor and not project_is_fixed_fee then (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *50
-       when user_name = 'Mark Rittman' then (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *50
-       else (SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_billing_month )) *25 end)) , (coalesce((round(total_revenue * (total_hours_billed/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_month)),2)),0))) as consultant_margin_pct
-
+       COUNT(timesheet_billing_week) over (PARTITION BY timesheet_project_pk) as project_duration_weeks,
+       ROUND(SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,timesheet_billing_week)) AS total_project_hours_billed,
+       SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk, timesheet_task_pk,timesheet_billing_week ) as consultant_hours_billed,
+       CASE WHEN user_is_contractor AND project_is_fixed_fee then ((project_fee_amount/(count(timesheet_billing_week) over (PARTITION BY timesheet_project_pk, user_pk, timesheet_task_pk,timesheet_billing_week)))/2) * SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk, timesheet_task_pk,timesheet_billing_week ) / ROUND(SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,user_pk))
+            ELSE ((SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk, timesheet_task_pk,timesheet_billing_week )) * user_cost_rate) end as consultant_cost,
+       SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk,timesheet_task_pk,timesheet_billing_week )/SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk) AS consultant_share_hours_billed_pct,
+       ROUND(SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,user_pk)) AS total_consultant_project_hours_billed,
+       SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk, user_pk, timesheet_task_pk,timesheet_billing_week ) / ROUND(SUM(total_hours_billed) OVER (PARTITION BY timesheet_project_pk,user_pk)) as consultant_share_consultant_hours_billed_pct
 
 FROM (
   SELECT
     c.company_pk,
-    date_trunc(date(timesheet_billing_date),MONTH) as timesheet_billing_month,
+    date_trunc(date(timesheet_billing_date),WEEK) as timesheet_billing_week,
     company_name,
     company_description,
     company_currency_code,
     t.timesheet_project_pk,
     project_name,
+    t.timesheet_task_pk,
     a.task_name,
     project_code,
     project_delivery_start_ts,
@@ -89,6 +75,7 @@ FROM (
     t.user_pk,
     user_name,
     user_is_contractor,
+    coalesce(u.user_cost_rate,25) as user_cost_rate,
     SUM(t.timesheet_hours_billed) AS total_hours_billed,
     coalesce(project_fee_amount,
       0)+SUM(timesheet_total_amount_billed) AS total_revenue
@@ -133,8 +120,8 @@ FROM (
     16,
     17,
     18,
-  19,20)
+  19,20,21,22)
     )
-group by 1,2,3,4,5,6,7,8,9)
+group by 1,2,3,4,5,6,7,8,9,10)
 select *
 from report
