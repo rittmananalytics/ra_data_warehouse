3394bf7793d38bd50f8b2f33f5484e55926c3620
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-12-11 22:58:27 +0000

    more hubspot

--- models/integration/int_ad_campaign_performance.sql
+++ models/integration/int_ad_campaign_performance.sql
@@ -20,7 +20,14 @@ with ad_campaigns as
     SELECT *
     FROM   {{ ref('stg_mailchimp_email_ad_campaign_performance') }}
     {% endif %}
-    
+    {% if (var("enable_facebook_ads_source") or var("enable_google_ads_source") or var("enable_mailchimp_email_source")) and  var("enable_hubspot_crm_source") %}
+    UNION All
+    {% endif %}
+    {% if var("enable_hubspot_crm_source")  %}
+    SELECT *
+    FROM   {{ ref('stg_hubspot_crm_ad_campaign_performance') }}
+    {% endif %}
+
 
   )
 select * from ad_campaigns
